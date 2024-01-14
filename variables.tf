595cb1b2c82d67b793b057f0a2f67bfd1bdf8c46
webpwnized <webpwnized@gmail.com>
2024-01-14 17:18:34 -0500

    0.0.4 Added LB and WAF to Cloud Run

--- variables.tf
+++ variables.tf
@@ -15,12 +15,6 @@ variable "project" {
 	description	= "The GCP project in which the assets will be built"
 }
 
-variable "ssh-public-key-file" {
-	type 		= string
-	default 	= "/home/jeremy/.ssh/gcp-ssh-key.pub"
-	description	= "The SSH public key you will use to authenticate to the IaaS bastion host via GCP Identity Aware Proxy and the Docker Host using SSH"
-}
-
 variable "default-labels" { 
 	type = map(string)
 	default = {
@@ -31,35 +25,12 @@ variable "default-labels" {
 	description	= "The labels that will be applied to the IaaS assets"
 }
 
-variable "admin-office-ip-address-range" {
-	type = list(string)
-	default = ["104.0.151.118/32"]
-	description	= "Your IP address. Terraform will set up firewall rules allowing access from this range."
-}
-
-variable "database-username-secret" {
-	type 		= string
-	default 	= "dragonfly-database-username"
-	description	= "The name of the GCP secret that stores the database username the dragonfly application uses to authenticate to the Cloud SQL database. Because the value is sensitive, the value must be stored in GCP Secret Manager. See https://cloud.google.com/secret-manager/docs/creating-and-accessing-secrets"
-}
-
-variable "database-password-secret" {
-	type 		= string
-	default 	= "dragonfly-database-password"
-	description	= "The name of the GCP secret that stores the database password the dragonfly application uses to authenticate to the Cloud SQL database. Because the value is sensitive, the value must be stored in GCP Secret Manager. See https://cloud.google.com/secret-manager/docs/creating-and-accessing-secrets"
-}
-
 //########################################################################
 //	The default value of these varaibles may be used or changed.
 //	The default value is probably fine.
 //########################################################################
 
 variable "application-name" {
-	type = string
-	default = "dragonfly"
-}
-
-variable "dragonfly-application-name" {
 	type = string
 	default = "dragonfly"
 }
@@ -74,29 +45,9 @@ variable "zone" {
 	default = "us-central1-a"
 }
 
-variable "bastion-host-subnet-ip-address-range" {
-	type = string
-	default = "10.0.0.0/28"
-}
-
 variable "cloud-run-service-subnet-ip-address-range" {
 	type = string
 	default = "10.0.1.0/28"
-}
-
-variable "gcp-iap-ip-address-range" {
-	type = list(string)
-	default = ["35.235.240.0/20"]
-}
-
-variable "gcp-health-check-ip-address-range" {
-	type = list(string)
-	default = ["130.211.0.0/22", "35.191.0.0/16"]
-}
-
-variable "ssh-port" {
-	type = string
-	default = "22"
 }
 
 variable "http-port" {
@@ -108,29 +59,3 @@ variable "https-port" {
 	type = string
 	default = "443"
 }
-
-variable "ntp-port" {
-	type = string
-	default = "123"
-}
-
-variable "dragonfly-http-port" {
-	type = string
-	default = "80"
-}
-
-variable "vm-machine-type" {
-	type = string
-	default = "e2-small"
-}
-
-variable "vm-boot-disk-type" {
-	type = string
-	default = "pd-standard"
-}
-
-variable "vm-metadata-startup-script" {
-	type = string
-	default = "#! /bin/bash\nsudo apt update\nsudo apt -y install google-osconfig-agent"
-}
-
