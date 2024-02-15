50009a36d597b39f5666fa735da50ebeb0df5417
webpwnized <webpwnized@gmail.com>
2024-02-15 10:01:41 -0500

    1.0.0 Bug Fix: Leave Service Usage API enabled

--- project-services.tf
+++ project-services.tf
@@ -26,3 +26,16 @@ resource "google_project_service" "cloud-run-service" {
 	disable_on_destroy			= "true"
 	disable_dependent_services	= "true"
 }
+
+resource "google_project_service" "serviceusage-service" {
+	project						= "${var.project}"
+	service						= "serviceusage.googleapis.com"
+	# The service usage API is required to list enabled services, 
+	# which creates a chicken and egg problem. Terraform cannot 
+	# list available or enabled services with the Service Usage 
+	# API, which prevents Terraform from pre-checking the API as 
+	# part of the service enabling process. The Service Usage API 
+	# has to be left enabled. 
+	disable_on_destroy			= "false"
+	disable_dependent_services	= "false"
+}
